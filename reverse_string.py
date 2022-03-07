07f5d932436b8e8c2a5dbf39678135c8fbb49f22
Pravallika <96050882+prava-coder@users.noreply.github.com>
2022-03-07 09:53:00 +0530

    Add files via upload

---
+++ reverse_string.py
@@ -0,0 +1,12 @@
+#trick to reverse a string in python
+
+x="i love python"
+print(x[::-1])
+
+#reverse a string by using function
+
+def reverse_str(x):
+    return x[::-1]
+my_text=reverse_str("Hello,I am pravalli")
+print(my_text)
+
